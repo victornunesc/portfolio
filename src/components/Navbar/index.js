d2198aec0341d5c31d14e5196f6548b33693cdfb
Victor <victornunesc95@gmail.com>
2021-12-21 11:52:11 -0300

    feat: navigation bar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavBtn,
+  NavBtnLink,
+  NavItem,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+} from "./style";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">dolla</NavLogo>
+        <MobileIcon>
+          <FaBars />
+        </MobileIcon>
+        <NavMenu>
+          <NavItem>
+            <NavLinks to="about">About</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="discover">Discover</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="services">Services</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="signup">Signup</NavLinks>
+          </NavItem>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In</NavBtnLink>
+        </NavBtn>
+      </NavbarContainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
